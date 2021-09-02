699c87308d1e8828a249701b28ecef39742feafb
Travis Yatsko <travis@balouapp.com>
2021-09-02 14:05:59 -0700

    fixe for dropdown button drawing below container.
    background of phone switched to white
    
    menu shape switched to rounded rectangle

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -25,157 +25,111 @@ class _HomeScreenState extends State<HomeScreen> {
     double _phoneWidth = MediaQuery.of(context).size.width * 0.5;
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent.shade200,
-      // appBar: AppBar(
-      //   leading: IconButton(
-      //     icon: Icon(Icons.menu),
-      //     onPressed: () => print(
-      //       'hamburger pressed',
-      //     ),
-      //   ),
-      //   elevation: 0,
-      //   backgroundColor: Colors.transparent,
-      // ),
       body: Center(
         child: Container(
           height: _phoneHeight,
           width: _phoneWidth,
+          clipBehavior: Clip.none,
           decoration: ShapeDecoration(
-            //color: Colors.black,
+            color: Colors.white,
             shape: RoundedRectangleBorder(
               side: BorderSide(color: _phoneColor, width: 20),
               borderRadius: BorderRadius.all(Radius.circular(40)),
             ),
           ),
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.03,
-              width: MediaQuery.of(context).size.width * 0.2,
-              decoration: ShapeDecoration(
-                color: _phoneColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(15),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  clipBehavior: Clip.none,
+                  height: MediaQuery.of(context).size.height * 0.03,
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  decoration: ShapeDecoration(
+                    color: _phoneColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(15),
+                      ),
+                    ),
                   ),
                 ),
               ),
-              child: OverflowBox(
-                maxHeight: _phoneHeight - 20,
-                maxWidth: _phoneWidth - 20,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        height: 50,
-                        width: 50,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(100),
-                          child: Image.network(
-                            'https://portfolium1.cloudimg.io/s/crop/192x192/https://cdn.portfolium.com/ugcs3%2Favatar%2F9A1773AD-1F85-4322-2DC3-624E08F24D42-0-xKXrfFsi-JV-pl2eZkIOEQB7zWw3M92eZXsgd6k3jjoGjcY6V85pwuwDVB7.jpg',
-                          ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.network(
+                          'https://portfolium1.cloudimg.io/s/crop/192x192/https://cdn.portfolium.com/ugcs3%2Favatar%2F9A1773AD-1F85-4322-2DC3-624E08F24D42-0-xKXrfFsi-JV-pl2eZkIOEQB7zWw3M92eZXsgd6k3jjoGjcY6V85pwuwDVB7.jpg',
                         ),
                       ),
-                      PopupMenuButton<MenuActions>(
-                        icon: Icon(
-                          FontAwesomeIcons.bars,
-                          color: Colors.black87,
-                        ),
-                        itemBuilder: (BuildContext context) =>
-                            <PopupMenuEntry<MenuActions>>[
-                          const PopupMenuItem(
-                            child: Text('Projects'),
-                            value: MenuActions.projects,
-                          ),
-                          const PopupMenuItem(
-                            child: Text('Resume'),
-                            value: MenuActions.resume,
-                          ),
-                          const PopupMenuItem(
-                            child: Text('LinkedIn'),
-                            value: MenuActions.linkedin,
-                          ),
-                        ],
-                        onSelected: (MenuActions result) {
-                          switch (result) {
-                            case MenuActions.projects:
-                              {
-                                print(result);
-                                // setState(() {
-                                //   actionToPass = MenuActions.about;
-                                // });
-                              }
-                              break;
-                            case MenuActions.resume:
-                              {
-                                print(result);
-                                // setState(() {
-                                //   actionToPass = MenuActions.pool;
-                                // });
-                              }
-                              break;
-                            case MenuActions.linkedin:
-                              {
-                                print(result);
-                                // setState(() {
-                                //   actionToPass = MenuActions.guide;
-                                // });
-                              }
-                              break;
-                          }
-                        },
-                      ),
-                    ],
-                  ),
+                    ),
+                    hamburger(context),
+                  ],
                 ),
               ),
-            ),
+            ],
           ),
         ),
       ),
-      // Row(
-      //   children: [
-      //     Padding(
-      //       padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.start,
-      //         crossAxisAlignment: CrossAxisAlignment.center,
-      //         children: <Widget>[
-      //           ClipRRect(
-      //             borderRadius: BorderRadius.circular(100),
-      //             child: Image.network(
-      //               'https://portfolium1.cloudimg.io/s/crop/192x192/https://cdn.portfolium.com/ugcs3%2Favatar%2F9A1773AD-1F85-4322-2DC3-624E08F24D42-0-xKXrfFsi-JV-pl2eZkIOEQB7zWw3M92eZXsgd6k3jjoGjcY6V85pwuwDVB7.jpg',
-      //             ),
-      //           ),
-      //           SizedBox(
-      //             height: 10,
-      //           ),
-      //           Column(
-      //             crossAxisAlignment: CrossAxisAlignment.start,
-      //             children: [
-      //               Text(
-      //                 'Travis Yatsko',
-      //                 style: TextStyle(
-      //                     color: Colors.white, fontWeight: FontWeight.bold),
-      //               ),
-      //               Text(
-      //                 'Program Manager & Mobile Developer',
-      //                 style: TextStyle(
-      //                   color: Colors.white,
-      //                 ),
-      //               ),
-      //             ],
-      //           )
-      //         ],
-      //       ),
-      //     ),
-      //     VerticalDivider(
-      //       thickness: 4,
-      //       color: Colors.white,
-      //     )
-      //   ],
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget hamburger(BuildContext context) {
+    return PopupMenuButton<MenuActions>(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      icon: Icon(
+        FontAwesomeIcons.bars,
+        color: Colors.black87,
+      ),
+      itemBuilder: (context) => <PopupMenuEntry<MenuActions>>[
+        const PopupMenuItem(
+          child: Text('Projects'),
+          value: MenuActions.projects,
+        ),
+        const PopupMenuItem(
+          child: Text('Resume'),
+          value: MenuActions.resume,
+        ),
+        const PopupMenuItem(
+          child: Text('LinkedIn'),
+          value: MenuActions.linkedin,
+        ),
+      ],
+      onSelected: (MenuActions result) {
+        switch (result) {
+          case MenuActions.projects:
+            {
+              print(result);
+              // setState(() {
+              //   actionToPass = MenuActions.about;
+              // });
+            }
+            break;
+          case MenuActions.resume:
+            {
+              print(result);
+              // setState(() {
+              //   actionToPass = MenuActions.pool;
+              // });
+            }
+            break;
+          case MenuActions.linkedin:
+            {
+              print(result);
+              // setState(() {
+              //   actionToPass = MenuActions.guide;
+              // });
+            }
+            break;
+        }
+      },
     );
   }
 }
